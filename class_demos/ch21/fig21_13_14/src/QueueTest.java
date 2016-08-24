8b3cf72d0f083615947af4e0cca69e4771e209f2
Hans Johnson <hans-johnson@uiowa.edu>
2016-08-24 17:21:04 -0500

    ENH: Added initial skelton files

--- class_demos/ch21/fig21_13_14/src/QueueTest.java
+++ class_demos/ch21/fig21_13_14/src/QueueTest.java
@@ -4,6 +4,7 @@
 import com.deitel.datastructures.EmptyListException;
 import com.deitel.datastructures.Queue;
 
+
 public class QueueTest {
     public static void main(String[] args) {
         Queue<Integer> queue = new Queue<>();
