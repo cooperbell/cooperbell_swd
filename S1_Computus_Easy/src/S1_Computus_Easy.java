d51b80cc3bf7b93e906f48151f9e651dfb4378cd
Cooper Bell <cooper-bell@uiowa.edu>
2017-08-25 09:21:36 -0500

    Changed Java Programming to SWD

---
+++ S1_Computus_Easy/src/S1_Computus_Easy.java
@@ -0,0 +1,9 @@
+public class S1_Computus_Easy
+{
+
+    //Main method
+    public static void main(String[] args)
+    {
+
+    }
+}
--- class_demos/ch02/fig02_01/src/Welcome1.java
+++ class_demos/ch02/fig02_01/src/Welcome1.java
@@ -4,7 +4,7 @@
 public class Welcome1 {
     // main method begins execution of Java application
     public static void main(String[] args) {
-        System.out.println("Welcome to Java Programming!");
+        System.out.println("Welcome to SWD!");
     } // end method main
 } // end class Welcome1
 
